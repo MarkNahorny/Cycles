8a1fa27100842331e1db9bb91b7fe9b9bf1cb3c4
VitaliKukrash <koresh007@tut.by>
2021-08-29 19:03:54 +0300

    Initial commit

---
+++ src/org/itstep/Swing.java
@@ -0,0 +1,12 @@
+package org.itstep;
+
+import javax.swing.*;
+
+import static javax.swing.JOptionPane.*;
+
+public class Swing {
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null,"Hello world \n Привет мир");
+
+    }
+}
